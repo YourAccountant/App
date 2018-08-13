cc76a1b1b3cf8fd856f9dd179a82ade01cddde18
kobus <dev.kobus@gmail.com>
2018-08-13 20:53:16 +0200

    webpack

---
+++ public/js/bundle.js
@@ -0,0 +1,3 @@
+const path = require('path');
+
+require('../scss/bundle.scss');
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+module.exports = {
+    entry: "./public/js/bundle.js",
+    output: {
+        path: path.resolve(__dirname, 'public/dist'),
+        filename: "bundle.dist.js"
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+     plugins: [
+        new MiniCssExtractPlugin({
+            filename: "bundle.dist.css",
+            chunkFilename: "[id].css"
+        })
+    ]
+}
