a109dc1794e9d3a8df05578c9f429ebd70c216df
kobus <dev.kobus@gmail.com>
2018-09-05 21:45:35 +0200

    public things

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1,3 +1,38 @@
-const path = require('path');
-
 require('../scss/bundle.scss');
+
+$('#signin, #signup').on('submit', function(e) {
+    e.preventDefault();
+
+    let data = $(this).serializeArray()
+
+    let json = {}
+    data.forEach(d => {
+        json[d.name] = d.value;
+    })
+
+    let url = $(this).attr('action');
+
+    $.post(url, JSON.stringify(json), d => {
+        console.log('success', d);
+    })
+    .done(d => {
+        console.log('done', d);
+    })
+    .fail(d => {
+        console.log('success', d);
+    })
+});
+
+function getCookie(name) {
+  var value = "; " + document.cookie;
+  var parts = value.split("; " + name + "=");
+  if (parts.length == 2) return parts.pop().split(";").shift();
+}
+
+$('#get').on('click', function (e) {
+    // console.log(getCookie('token'));
+
+    $.get('/api/v1/client/1', d => {
+        console.log(d);
+    })
+})
