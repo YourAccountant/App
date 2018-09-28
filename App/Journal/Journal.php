e95442ce0bff9dbb8fa148a1d968d7eca28ee87e
kobus <dev.kobus@gmail.com>
2018-09-28 09:10:07 +0200

    init journal

---
+++ App/Journal/Journal.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Journal;
+
+use \Core\Foudation\Model;
+use \App\Util\Price;
+use \App\Account\Account;
+
+class Journal extends Model
+{
+    protected $table = "journals";
+
+    public function getJournalsFromAdministration($id)
+    {
+        return $this->getBuilder()
+            ->where('administration_id', '=', $id)
+            ->orderBy('`created_at` DESC')
+            ->exec()
+            ->fetchAll();
+    }
+}
---
+++ App/Journal/JournalController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Journal;
+
+use \Core\Foundation\Controller;
+
+class JournalController extends Controller
+{
+}
